e513219ff9e4c462388197e57c7b1f2e134ea27a
Damian Nicholson <damian.nicholson21@gmail.com>
2015-03-08 23:53:32 +0000

    Back to basics

--- main.go
+++ main.go
@@ -2,46 +2,74 @@ package main
 
 import (
   "fmt"
-  "io/ioutil"
+  "io"
+  "unicode/utf8"
 )
 
-type tokenType int
-
-type Token struct {
-  typ   tokenType
-  value string
-}
-
-func (token Token) String() string {
-  switch token.typ {
-  case tokenEOF:
-    return "EOF"
-  case tokenError:
-    return token.value
-  }
-  return fmt.Sprintf("%q", token.value)
-}
+type Token int
 
 const (
-  tokenComment tokenType = iota
-  tokenNewline
-  tokenWhitespace
-  tokenError
-  tokenEOF
+  TokenEOF Token = iota
+  TokenWhitespace
+  TokenColon
+  TokenSemicolon
+  TokenLeftCurly
+  TokenRightCurly
+  TokenString
 )
 
-func main() {
-  stylesheet := "test.css"
-  file, err := ioutil.ReadFile(stylesheet)
-  if err != nil {
-    fmt.Println("Error")
+type Lexer struct {
+  s string
+  i int64
+}
+
+func (l *Lexer) Next() (ch rune, size int, err error) {
+  if l.i >= int64(len(l.s)) {
+    return 0, 0, io.EOF
   }
-  strFile := string(file)
-  lexer := NewLexer(strFile)
+  ch, size = utf8.DecodeRuneInString(l.s[l.i:])
+  l.i += int64(size)
+  return
+}
+
+func (l *Lexer) String() string {
+  return fmt.Sprintf("str: %s, ind: %d", l.s, l.i)
+}
+
+func (l *Lexer) Lex() Token {
   for {
-    token := lexer.Next()
-    if token.typ == tokenEOF {
-      break
+    r, _, err := l.Next()
+    if err == io.EOF {
+      fmt.Println("EOF-TOKEN")
+      return TokenWhitespace
+    }
+    switch r {
+    case ' ', '\t', '\n':
+      fmt.Println("WHITESPACE-TOKEN")
+      return TokenWhitespace
+    case '{':
+      fmt.Println("{-TOKEN")
+      return TokenLeftCurly
+    case '}':
+      fmt.Println("}-TOKEN")
+      return TokenRightCurly
+    case ':':
+      fmt.Println("COLON-TOKEN")
+      return TokenColon
+    case ';':
+      fmt.Println("SEMICOLON-TOKEN")
+      return TokenSemicolon
+    default:
+      fmt.Println("OTHER-TOKEN")
+      return TokenString
     }
   }
+}
+
+func NewLexer(s string) *Lexer { return &Lexer{s, 0} }
+
+func main() {
+  str := "body { background-color: red; }"
+  lexer := NewLexer(str)
+  lexer.Lex()
 }
